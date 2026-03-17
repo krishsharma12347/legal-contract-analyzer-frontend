ca220982c4a985007288ee4ecdd6bce410f10659
krishsharma12347 <krishsharmaks32@gmail.com>
2026-03-17 11:14:11 +0530

    last

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,6 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),tailwindcss()],
-// })
-
-
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: ['vite']
-    }
-  }
-})
+  plugins: [react()],
+});
